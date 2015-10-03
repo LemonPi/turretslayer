e3e477a1e8d163e613d4ce4a3df474670d069b93
yaroslavb1 <yaroslav.babich@gmail.com>
2015-10-03 17:38:39 -0600

    Sees walls

--- PlayerAI.py
+++ PlayerAI.py
@@ -5,8 +5,22 @@ from PythonClientAPI.libs.Game.MapOutOfBoundsException import *
 class PlayerAI:
     def __init__(self):
         # Initialize any objects or variables you need here.
+        self.walls = None
         pass
+
+    def calc_walls(self, gameboard):
+        self.walls = [[False for y in range(gameboard.height)] for x in range(gameboard.width)]
+        for wall in gameboard.walls:
+            self.walls[wall.x][wall.y] = True
+        for turret in gameboard.turrets:
+            self.walls[turret.x][turret.y] = True
 
     def get_move(self, gameboard, player, opponent):
         # Write your AI here.
+        if self.walls == None:
+            self.calc_walls(gameboard)
+        print(self.walls)
+        turn = gameboard.current_turn
+
+        
         return Move.NONE
